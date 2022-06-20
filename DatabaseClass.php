d8dc65d59bf09bf20d9452ac3787fa834a6b873f
Ducugnon Thomas <thomas.ducugnons@gmail.com>
2022-06-20 10:11:43 +0200

    marche pas

---
+++ DatabaseClass.php
@@ -0,0 +1,43 @@
+<?php
+Class Database {
+
+    private $user = 'root';
+    private $password = '';
+
+    public function __construct($user, $password) {
+        $this->user = $user;
+        $this->password = $password;
+    }
+    public function getUser() {
+        return $this->user;
+    }
+    public function getPassword() {
+        return $this->password;
+    }
+    public function setUser($user) {
+        $this->user = $user;
+    }
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+
+    public function connect($bdd, $table) {
+
+        try {
+            $dbh = new PDO('mysql:host=localhost;dbname='.$bdd, $this->user, $this->password);
+            $query = $dbh->prepare('SELECT Postcode from '.$table.' LIMIT 50');
+            $query->execute();
+            $result = $query->fetchAll();
+            return $result;
+        } catch (PDOException $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+
+    }
+
+}
+
+
+?>
---
+++ main.php
@@ -0,0 +1,121 @@
+<?php
+include('RequestClass.php');
+include('DatabaseClass.php');
+require 'vendor/autoload.php';
+use GuzzleHttp\Promise;
+use GuzzleHttp\Client;
+
+ini_set('max_execution_time', 0);
+
+$connect = new Database('root', '');
+
+$postCodeHost = $connect->connect('databasetflapi', 'host_database');
+$postCodeUni = $connect->connect('databasetflapi', 'university_database');
+
+$tableAllRequest = [];
+
+$client = new Client(['base_uri' => 'https://api.tfl.gov.uk/']);
+
+$promises = [];
+
+$requestSent = 0;
+
+foreach ($postCodeUni as $uniPostCode) {
+
+    $uni = ltrim(utf8_encode($uniPostCode['Postcode']));
+    //var_dump($uni);
+
+    foreach ($postCodeHost as $hostPostCode) {
+
+        $host = ltrim(utf8_encode($hostPostCode['Postcode']));
+        //var_dump($host);
+        
+        $promises[$uni . " and ". $host] = $client->getAsync('/journey/journeyresults/' . $uni . '/to/' . $host . '?app_key=a59c7dbb0d51419d8d3f9dfbf09bd5cc');
+
+        $requestSent++;
+        var_dump($requestSent);
+        if ($requestSent == 499) {
+            sleep(60);
+            $requestSent = 0;
+        }
+        //usleep(100000);
+       
+    }
+}
+
+$results = Promise\settle($promises)->wait();
+
+foreach($results as $promiseKey => $result) {
+    //var_dump($result);
+
+    if ($result['state'] == 'fulfilled') {
+        if ($result['value']->getStatusCode() == 200) {
+            $json = json_decode($result['value']->getBody()->getContents(), true);
+            $journeys = $json['journeys'];
+        
+            $durations = [];
+        
+            //var_dump($promiseKey);
+            //var_dump($journeys);
+    
+            foreach ($journeys as $journey) {
+                $duration = $journey['duration'];
+                array_push($durations, $duration);
+            }
+        
+            $min = min($durations);
+        
+            $tableAllRequest[$promiseKey] = $min;
+            
+        }
+    }
+}
+
+var_dump($tableAllRequest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//.$this->postCode.
+        // $host = $hostPostCode['Postcode'];
+
+        // $data =  json_decode(file_get_contents('https://api.tfl.gov.uk/journey/journeyresults/' . $uni . '/to/' . $host . '?app_key=a59c7dbb0d51419d8d3f9dfbf09bd5cc'), true);
+
+        // $duration = $data['journeys'];
+
+        
+        // //$datas = file_get_contents('https://api.tfl.gov.uk/journey/journeyresults/UB68BP/to/W138ER');
+
+
+        // //echo $datas."<br>";
+
+        // $tableTemp = [];
+
+        // foreach ($duration as $durations) {
+        //     $durationns = $durations['duration'];
+        //     array_push($tableTemp, $durationns);
+        // }
+
+        // $min = min($tableTemp);
+
+        // echo "Shorter travel time : " . $min . " of travel between " . $uni . " and ". $host . " . <br>";
+
+        // echo "<br>";
+
+        // //$request = new Request($uniPostCode['Postcode'], $hostPostCode['Postcode']);
+        // array_push($tableAllRequest, array($uni . " and ". $host => $min));
