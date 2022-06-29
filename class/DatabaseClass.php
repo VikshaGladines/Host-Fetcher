b6725d2f4337ef2ad241f627832ca6e53dd0a6b1
Ducugnon Thomas <thomas.ducugnons@gmail.com>
2022-06-29 11:24:56 +0200

    add documentation DataBaseClass

--- class/DatabaseClass.php
+++ class/DatabaseClass.php
@@ -1,9 +1,10 @@
 <?php
 class Database
 {
-
+    // Attribute that represente the database connection
     private $dbh;
 
+    // Constructor to create a connection to the database
     public function __construct($user, $password, $bdd)
     {
         $this->dbh = new PDO('mysql:host=localhost;dbname=' . $bdd, $user, $password);
@@ -25,11 +26,17 @@ class Database
         $this->password = $password;
     }
 
-
+    /**
+     * documented function
+     *
+     * @param string $table
+     * @return array $result with all the Postcode from tables
+     */
     public function load($table)
     {
 
         try {
+            // MySql request select to select all Postcode from the tables
             $query = $this->dbh->prepare('SELECT Postcode from ' . $table . ' LIMIT 20');
             $query->execute();
             $result = $query->fetchAll();
@@ -40,10 +47,19 @@ class Database
         }
     }
 
+    /**
+     * documented function
+     *
+     * @param string $table
+     * @return void
+     * 
+     * Function to empty a table
+     */
     public function truncate($table)
     {
         try {
 
+            // MySql request to empty the table  
             $query = $this->dbh->prepare('TRUNCATE TABLE ' . $table);
             $query->execute();
         } catch (PDOException $e) {
@@ -51,10 +67,20 @@ class Database
             die();
         }
     }
-
+    /**
+     * documented function
+     *
+     * @param string $table
+     * @param string $whereCol
+     * @param string $condition
+     * @return void
+     * 
+     * function to delete line from the table you want
+     */
     public function delete($table, $whereCol, $condition)
     {
         try {
+            // MySql request to delete from a table the line you want
             $query = $this->dbh->prepare("DELETE FROM " . $table . " WHERE " . $whereCol . " = '" . $condition . "'");
             $query->execute();
         } catch (PDOException $e) {
@@ -62,10 +88,21 @@ class Database
             die();
         }
     }
-
+    /**
+     * documented function
+     *
+     * @param string $table
+     * @param string $uni
+     * @param string $host
+     * @param int $time
+     * @return void
+     * 
+     * function to update the table
+     */
     public function update($table, $uni, $host, $time)
     {
         try {
+            // MySql request to insert a the new value
             $query = $this->dbh->prepare('INSERT INTO ' . $table . ' (HostPostCode, UniPostCode, TravelTime) VALUES (?,?,?)');
             $query->execute([$host, $uni, $time]);
         } catch (PDOException $e) {
@@ -73,10 +110,19 @@ class Database
             die();
         }
     }
-
+    /**
+     * documented function
+     *
+     * @param string $table
+     * @param string $selection
+     * @return array $result
+     * 
+     * Function to select all the information from a table 
+     */
     public function selectAll($table, $selection)
     {
         try {
+            // MySql request to select all the value from a table
             $query = $this->dbh->prepare('SELECT ' . $selection . ' from ' . $table);
             $query->execute();
             $result = $query->fetchAll();
@@ -86,10 +132,23 @@ class Database
             die();
         }
     }
-
+    /**
+     * documented function
+     *
+     * @param string $table
+     * @param string $selection
+     * @param string $colWhere
+     * @param string $where
+     * @param string $order
+     * @param string $colOrder
+     * @return array $result
+     * 
+     * Function to select value in a table where the values match and order it  
+     */
     public function selectWhere($table, $selection, $colWhere, $where, $order, $colOrder)
     {
         try {
+            // MySql request to select 10 value with a condition WHERE and ordered
             $query = $this->dbh->prepare('SELECT ' . $selection . ' from ' . $table . ' WHERE ' . $colWhere . " = '" . $where . "' ORDER BY " . $colOrder . ' ' . $order . " LIMIT 10");
             $query->execute();
             $result = $query->fetchAll();
@@ -99,7 +158,16 @@ class Database
             die();
         }
     }
-
+    /**
+     * documented function
+     *
+     * @param string $table
+     * @param string $selection
+     * @param string $value
+     * @return string $result
+     * 
+     * Function to select value from a table with multiply conditions WHERE 
+     */
     public function selectWhereOr($table, $selection, $value)
     {
         try {
