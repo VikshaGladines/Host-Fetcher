2c23f5fcd0b06eed250d10556a0455698cb05c51
richardCotte <rcotte1582@gmail.com>
2022-06-30 11:03:05 +0200

    Adding body to updateScript

--- scripts/updateBase.php
+++ scripts/updateBase.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loading...</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
 include('../class/DatabaseClass.php');
 require 'vendor/autoload.php';
@@ -231,3 +244,5 @@ function processTravel($promises)
     $promises = [];
     return [$promises, $tableRequest, $tableError];
 }
+
+?>
